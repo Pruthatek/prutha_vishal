9adf730894433bc14d366f1fbe6fcb4e7cfa1cd8
Ansh Modi <121238681+Anshmodi30@users.noreply.github.com>
2023-04-13 09:06:53 +0000

    ...

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import Home_img from "./Home_img";
 const Home = () => {
   return (
     <div className="text-white px-20 mt-32 flex items-center justify-between mb-20">
-      <div className="w-[60%]">
+      <div className="">
         <p className="text-[40px]">Your Idea Our Code</p>
         <p className="mt-10 w-[65%] text-justify">
           Lorem ipsum dolor sit amet, consectetuer adipiscing elit, Lorem ipsum
@@ -13,7 +13,7 @@ const Home = () => {
           adipiscing elit
         </p>
       </div>
-      <div className="bg-blue-900/30 rounded-full">
+      <div className="bg-blue-900/30 rounded-full hidden lg:block">
         <div className="backdrop-blur-3xl">
           <Home_img />
         </div>
